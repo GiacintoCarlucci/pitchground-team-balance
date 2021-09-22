831f21620c078f042d40c588e4b0f38ec37b71b4
giacintocarlucci <Giacinto1996@live.it>
2021-09-22 13:58:49 +0200

    new version 0.0.2
    - added icons
    - simplified try/catch block
    - modified console color to match icon
    - removed console background color
    - updated manifest

--- content.js
+++ content.js
@@ -1,17 +1,13 @@
+console.log("%c[pitchground-team-balance] waiting for data loading...",'color:#ec3646')
+setTimeout(function(){
 try{
-  console.log("%c[pitchground-team-balance] waiting for data loading...",'background:#222; color:#ED1C24')
-  setTimeout(function(){
-    try{
-      const balanceXPath = 'div.ant-col:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > span:nth-child(2) > span:nth-child(1)'
-      const balanceDiv = document.querySelector(balanceXPath)
-      let balance = balanceDiv.innerText
-      let teamBalance = balance*40/100
-      balanceDiv.innerText += ` (\$ ${teamBalance})`
-      console.log("%c[pitchground-team-balance] team balance: ",'background:#222; color:#ED1C24', `\$ ${teamBalance}`)
-    }catch(e){
-      console.log('error in timeout function: ', e)
-    }
-  },7000)
-} catch(e) {
-  console.log('error in finding pitchground balance: ', e)
+  const balanceXPath = 'div.ant-col:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > span:nth-child(2) > span:nth-child(1)'
+  const balanceDiv = document.querySelector(balanceXPath)
+  let balance = balanceDiv.innerText
+  let teamBalance = balance*40/100
+  balanceDiv.innerText += ` (\$ ${teamBalance})`
+  console.log("%c[pitchground-team-balance] team balance: ",'color:#ec3646', `\$ ${teamBalance}`)
+}catch(e){
+  console.log('error in finding total balance: ', e)
 }
+},7000)
