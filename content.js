66e018b728dd8336ee2c05208b78307722ceac67
giacintocarlucci <Giacinto1996@live.it>
2021-11-02 15:30:25 +0100

    [fix] parsing comma separated numbers
    example: $ 1,236

--- content.js
+++ content.js
@@ -4,6 +4,7 @@ try{
   const balanceXPath = 'div.ant-col:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > span:nth-child(2) > span:nth-child(1)'
   const balanceDiv = document.querySelector(balanceXPath)
   let balance = balanceDiv.innerText
+  balance = parseFloat(balance.replace(',',''));
   let teamBalance = balance*40/100
   balanceDiv.innerText += ` (\$ ${teamBalance})`
   console.log("%c[pitchground-team-balance] team balance: ",'color:#ec3646', `\$ ${teamBalance}`)
